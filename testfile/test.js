ad99c267ec37a78259786bcf634c4c261abb1b7d
DansAlto <98467778+DansAlto@users.noreply.github.com>
2023-08-02 14:14:52 +0200

    Update test.js

--- testfile/test.js
+++ testfile/test.js
@@ -1,1 +1,11 @@
-Test = "hi"
+let random = Math.floor(Math.random() * (4 - 1) + 1);
+
+if(random === 1) {
+    Test = "E";
+} else if(random === 2) {
+    Test = "?";
+} else if(random === 3) {
+    Test = "O-o";
+} else if(random === 4) {
+    Test = "tired";
+}
