baf49dffd6aa77eb2df1a4de31cf637b4e005269
DansAlto <98467778+DansAlto@users.noreply.github.com>
2023-08-02 14:18:01 +0200

    Update test.js

--- testfile/test.js
+++ testfile/test.js
@@ -1,13 +1,1 @@
-localStorage.setItem(`test`,
-    let random = Math.floor(Math.random() * (4 - 1) + 1);
 
-    if (random === 1) {
-        Test = "E";
-    } else if (random === 2) {
-        Test = "?";
-    } else if (random === 3) {
-        Test = "O-o";
-    } else if (random === 4) {
-        Test = "tired";
-    }
-);
