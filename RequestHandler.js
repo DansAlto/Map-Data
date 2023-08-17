9b78d0927f83bf4affa061d0e6312b94cf2b7114
DansAlto <98467778+DansAlto@users.noreply.github.com>
2023-08-17 15:09:36 +0200

    Update RequestHandler.js

--- RequestHandler.js
+++ RequestHandler.js
@@ -2,9 +2,9 @@ let cover = document.getElementById(`cover`);
 
 var root = document.querySelector(':root');
 
-root.style.setProperty('--cube', '#1c2ca680');
-root.style.setProperty('--cube2', '#101a6a80');
-root.style.setProperty('--cube3', '#09114e80');
+root.style.setProperty('--cube', '#0026ff80');
+root.style.setProperty('--cube2', '#00188380');
+root.style.setProperty('--cube3', '#01086380');
 
 function FetchData(Key) {
     if (Key.includes('.js')) {
