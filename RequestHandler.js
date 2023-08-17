6f479b1a9104b197a859304836618119c814cfd9
DansAlto <98467778+DansAlto@users.noreply.github.com>
2023-08-17 14:31:42 +0200

    Update RequestHandler.js

--- RequestHandler.js
+++ RequestHandler.js
@@ -1,24 +1,37 @@
+let cover = document.getElementById(`cover`);
+
+var root = document.querySelector(':root');
+
+root.style.setProperty('--cube', '#1c2ca680');
+root.style.setProperty('--cube2', '#101a6a80');
+root.style.setProperty('--cube3', '#09114e80');
+
 function FetchData(Key) {
-    if(Key.includes('.js')) {
+    if (Key.includes('.js')) {
         fetch(`https://api.github.com/repos/DansAlto/Cubes/contents/${Key}`)
-        .then((res) => res.json())
-        .then((data) => {
-            fetch(`https://api.github.com/repos/DansAlto/Cubes/git/blobs/${data.sha}`)
-                .then((rep) => rep.json())
-                .then((filedata) => {
-                    let dd = atob(filedata.content)
-                    eval(dd);
-                })
-        });
-    } else if(Key.includes(`.png`) || Key.includes(`.jpg`)){
+            .then((res) => res.json())
+            .then((data) => {
+                fetch(`https://api.github.com/repos/DansAlto/Cubes/git/blobs/${data.sha}`)
+                    .then((rep) => rep.json())
+                    .then((filedata) => {
+                        let dd = atob(filedata.content)
+                        eval(dd);
+                    })
+            });
+    } else if (Key.includes(`.png`) || Key.includes(`.jpg`)) {
         fetch(`https://api.github.com/repos/DansAlto/Cubes/contents/${Key}`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
+            .then((res) => res.json())
+            .then((data) => {
+                fetch(`https://api.github.com/repos/DansAlto/Cubes/contents/${Key}`)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        cover.style.backgroundImage = `url('${data.download_url}')`;
+                    })
+            })
 
     }
 }
 
 // Requests!
 FetchData(`scripts/snake.js`);
+FetchData(`backgrounds/sleepy curtains.jpg`);
